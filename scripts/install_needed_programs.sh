70f14069024c3fee4be66efebe03781ce1a14dde
fuomag9 <fuomag@gmail.com>
2021-06-11 17:53:26 +0200

    new file:   scripts/install_needed_programs.sh 	new file:   scripts/start_caronte.sh 	new file:   scripts/start_flag_submitter.sh 	new file:   scripts/start_netdata.sh
    added base script for stuff

---
+++ scripts/install_needed_programs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release\
+    git -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt install python3-pip -y
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo pip3 install docker-compose --upgrade
---
+++ scripts/start_netdata.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -d "./docker-stuff-ipv6-ad" ]
+then
+    cd ./netdata
+    sudo docker-compose up -d
+else
+    echo "Error: Directory ./docker-stuff-ipv6-ad does not exists."
+fi
